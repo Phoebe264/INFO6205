bc575dd09b4884da6ae95a8a4327b0cd9bbeb7e7
li.tong3 <li.tong3@northeastern.edu>
2021-04-29 19:19:38 -0700

    Completed

---
+++ src/FinalExam/Fibonacci.js
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public int fib(int n){
+        if(n<=1){
+            return n;
+        }else{
+            return this.fib(n-1) + this.fib(n-2);
+        }
+
+}
+    public static void main(String[] args) {
+        Solution solObj = new Solution();
+        System.out.println(solObj.fib(4));
+    }
+}
